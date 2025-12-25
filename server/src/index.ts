5ca0b50151f25bda0f436d5b064a5c3f461196fa
Hector Ha <duonghv19909.code@gmail.com>
2025-12-25 16:28:33 -0500

    fix: add env configuration and logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,6 @@
 import app from "./app";
+import { config } from "./config";
 
-// Hardcoded port, no env yet
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
 });
