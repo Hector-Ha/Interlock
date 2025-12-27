0a64528e8e374d694a173dfe063766adaf833c2f
Hector Ha <duonghv19909.code@gmail.com>
2025-12-27 13:22:15 -0500

    fix(prisma): ensure env vars are loaded in config

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,7 +1,15 @@
+import "dotenv/config";
 import path from "path";
 import { defineConfig } from "prisma/config";
 
+console.log(
+  "Loading config. DB URL:",
+  process.env.DATABASE_URL ? "FOUND" : "MISSING"
+);
 export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL as string,
+  },
   earlyAccess: true,
   schema: path.join(__dirname, "prisma/schema.prisma"),
   migrate: {
