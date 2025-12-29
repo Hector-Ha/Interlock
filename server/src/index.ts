9719747a3a6fa60f74d9824009795b9f6822b1f3
Hector Ha <duonghv19909.code@gmail.com>
2025-12-29 10:35:22 -0500

    chore: refine configuration and update gitignore

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import "./instrument";
 import app from "./app";
-import { config } from "./config";
+import { config, validateEnv } from "./config";
+
+validateEnv();
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
