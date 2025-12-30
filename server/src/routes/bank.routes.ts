6148ec729fbb4338bfe79730117fb4849bbb66c2
Hector Ha <duonghv19909.code@gmail.com>
2025-12-30 11:38:47 -0500

    feat(api): register account routes

--- server/src/routes/bank.routes.ts
+++ server/src/routes/bank.routes.ts
@@ -1,15 +1,27 @@
-import { Router } from "express";
+import { Router, RequestHandler } from "express";
 import {
   linkBankWithDwolla,
   getBanks,
   initiateTransfer,
 } from "@/controllers/bank.controller";
+import {
+  getAccounts,
+  getAccountBalance,
+} from "@/controllers/account.controller";
 import { authenticate } from "@/middleware/auth";
 
 const router: Router = Router();
 
-router.get("/", authenticate, getBanks);
-router.post("/link-dwolla", authenticate, linkBankWithDwolla);
-router.post("/transfer", authenticate, initiateTransfer);
+router.get("/", authenticate, getBanks as RequestHandler);
+router.post("/link-dwolla", authenticate, linkBankWithDwolla as RequestHandler);
+router.post("/transfer", authenticate, initiateTransfer as RequestHandler);
+
+// Account Endpoints
+router.get("/:bankId/accounts", authenticate, getAccounts as RequestHandler);
+router.get(
+  "/:bankId/accounts/:accountId/balance",
+  authenticate,
+  getAccountBalance as RequestHandler
+);
 
 export default router;
